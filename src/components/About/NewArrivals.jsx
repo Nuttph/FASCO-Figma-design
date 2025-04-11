22cd1d2c0e13c2654dec22c3e3cc252ad0e332b9
Nuttaphon <nathn2420@gmail.com>
2025-04-11 14:54:56 +0700

    update product arrivals

--- src/components/About/NewArrivals.jsx
+++ src/components/About/NewArrivals.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import Container from "../layouts/Container";
 
+//icon
+import { FaStar } from "react-icons/fa";
+
+//image
+import i1 from "../../assets/arrivals/Images-1.png"
+import i2 from "../../assets/arrivals/Images-2.png"
+import i3 from "../../assets/arrivals/Images-3.png"
+import i4 from "../../assets/arrivals/Images-4.png"
+import i5 from "../../assets/arrivals/Images-5.png"
+import i6 from "../../assets/arrivals/Images.png"
+
 const NewArrivals = () => {
   const [options, setOptions] = useState([
     {
@@ -25,6 +36,33 @@ const NewArrivals = () => {
     },
   ]);
 
+  const products = [
+    {
+      img:i1,
+      productName:"Shiny Dress"
+    },
+    {
+      img:i2,
+      productName:"Long Dress"
+    },
+    {
+      img:i3,
+      productName:"Full Sweater"
+    },
+    {
+      img:i4,
+      productName:"White Dress"
+    },
+    {
+      img:i5,
+      productName:"Colorful Dress"
+    },
+    {
+      img:i6,
+      productName:"White Shirt"
+    },
+  ]
+
   return (
     <div className="my-[50px]">
       <Container>
@@ -44,12 +82,60 @@ const NewArrivals = () => {
                 key={index}
                 className={`${
                   item.focus ? "bg-black text-white" : "bg-[#FAFAFA] text-black"
-                } px-[30px] py-3 rounded-lg cursor-pointer`}
+                } px-[30px] py-3 rounded-lg cursor-pointer duration-300`}
+                onClick={()=>{
+                  setOptions((prev)=>
+                    prev.map((item,i)=>({
+                      ...item,
+                      focus:i === index,
+                    }))
+                  )
+                }}
               >
                 {item.title}
               </button>
             ))}
           </div>
+
+            {/* content */}
+            <div className="flex flex-wrap pt-4 flex-row justify-between gap-[50px]">
+
+              {products.map((item,index)=>(
+                <div key={index} className="w-[330px] flex flex-col shadow-2xl items-center px-4 py-6 rounded-lg">
+                <img src={item.img} alt={item.img} className="w-[350px] h-[#240] object-cover" />
+
+                <div className="flex flex-row justify-between w-full mt-3">
+                  <div className="flex flex-col">
+                    <h1>{item.productName}</h1>
+                    <p className="text-[10px] text-[#8A8A8A]">Al Karam</p>
+                  </div>
+                  <div className="text-yellow-300 flex flex-row">
+                    <FaStar/>
+                    <FaStar/>
+                    <FaStar/>
+                    <FaStar/>
+                    <FaStar/>
+                  </div>
+                </div>
+
+                <div className="text-[12px] w-full mt-4">
+                  (4.1k) Customer Reviews
+                </div>
+
+                <div className="w-full flex flex-row justify-between mt-4 items-center">
+                  <div className="text-[20px]">
+                    $95.50
+                  </div>
+                  <div className="text-[12px] text-red-500">
+                    Almost Sold Out
+                  </div>
+                </div>
+
+              </div>
+              ))}
+
+            </div>
+
         </div>
       </Container>
     </div>
